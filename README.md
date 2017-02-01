186fe7a78c47e8eaa51ee2b34b8e34bd3cdc4004
RealTimeCom <expertphp@gmail.com>
2017-02-01 14:36:13 +0200

    0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# rm-dir
-Remove / Delete all files and folders
+## rm-dir
+
+**Remove directory and all its contents, sync or async**
+
+```sh
+$ npm install rm-dir
+```
+#### Example
+```js
+const rmdir = require('rm-dir');
+
+rmdir('/dir/path/s-name'); // SYNC
+
+rmdir('/dir/path/a-name', e => console.log(e)); // ASYNC
+```
+--------------------------------------------------------
+**rm-dir** is licensed under the MIT license. See the included `LICENSE` file for more details.
---
+++ index.js
@@ -0,0 +1,55 @@
+/* SOURCE FILE - Copyright (c) 2017 rm-dir - Tanase Laurentiu Iulian - https://github.com/RealTimeCom/rm-dir */
+'use strict';
+
+const fs = require('fs'), s = require('path').sep;
+
+function each(d, a, cb) {
+    if (typeof cb === 'function') { // async
+        if (a.length > 0) {
+            fs.unlink(d + s + a[0], e => {
+                if (e) {
+                    if (e.code === 'EISDIR') {
+                        rmdir(d + s + a[0], e => {
+                            if (e) { cb(e); } else { each(d, a.splice(0, 1) ? a : a, cb); }
+                        });
+                    } else { cb(e); }
+                } else { each(d, a.splice(0, 1) ? a : a, cb); }
+            });
+        } else { fs.rmdir(d, e => cb(e)); }
+    } else { // sync
+        if (a.length > 0) {
+            try {
+                fs.unlinkSync(d + s + a[0]);
+                each(d, a.splice(0, 1) ? a : a);
+            } catch (e) {
+                if (e.code === 'EISDIR') {
+                    rmdir(d + s + a[0]);
+                    each(d, a.splice(0, 1) ? a : a);
+                } else { throw e; }
+            }
+        } else { fs.rmdirSync(d); }
+    }
+}
+
+function rmdir(d, cb) {
+    if (typeof cb === 'function') { // async
+        fs.rmdir(d, e => {
+            if (e) {
+                if (e.code === 'ENOTEMPTY') {
+                    fs.readdir(d, (e, a) => {
+                        if (e) { cb(e); } else { each(d, a, cb); }
+                    });
+                } else { cb(e); }
+            } else { cb(); }
+        });
+    } else { // sync
+        try {
+            fs.rmdirSync(d);
+        } catch (e) {
+            if (e.code === 'ENOTEMPTY') {
+                each(d, fs.readdirSync(d));
+            } else { throw e; }
+        }
+    }
+}
+module.exports = rmdir;
